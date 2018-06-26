3ae40a1d44242a6d3b42943f5c4dd2bf7cbbe4d4
DanielBouius <d.bouius@st.hanze.nl>
2018-06-26 16:52:30 +0000

    wordpress met docker zonder saltstack

--- syslogscript.sh
+++
@@ -1,7 +0,0 @@
-#!\bin\bash
-#Script om syslogserver IP toe te voegen aan de cliënt
-
-sudo echo " ###########################
-########## RULES ##########
-###########################
-*.* @10.0.1.4 " >> /etc/rsyslog.conf
